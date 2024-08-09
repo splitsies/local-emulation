07bf8e20e959755cc979e9aa2ddbbcb028d40ea1
Kevin Chen <kevchen21@gmail.com>
2024-08-09 12:44:22 -0700

    ExpenseGroup table

--- pg-tables.sql
+++ pg-tables.sql
@@ -12,3 +12,43 @@ CREATE TABLE "UserExpense" (
     "createdAt"         TIMESTAMPTZ,
     PRIMARY KEY("expenseId", "userId")
 );
+
+CREATE FUNCTION exists_as_child(id VARCHAR(36))
+RETURNS boolean AS $$
+DECLARE
+	rec_count INTEGER;
+BEGIN
+	SELECT COUNT(*)
+	  INTO rec_count
+	  FROM "ExpenseGroup"
+	 WHERE "childExpenseId" = id;
+
+	RETURN rec_count > 0;
+END; 
+$$
+LANGUAGE plpgsql;
+
+CREATE FUNCTION exists_as_parent(id VARCHAR(36))
+RETURNS boolean AS $$
+DECLARE
+	rec_count INTEGER;
+BEGIN
+	SELECT COUNT(*)
+	  INTO rec_count
+	  FROM "ExpenseGroup"
+	 WHERE "parentExpenseId" = id;
+
+	RETURN rec_count > 0;
+END; 
+$$
+LANGUAGE plpgsql;
+
+CREATE TABLE "ExpenseGroup" (
+    "parentExpenseId" VARCHAR(36) REFERENCES "Expense" ("id") ON DELETE CASCADE,
+    "childExpenseId" VARCHAR(36) REFERENCES "Expense" ("id") ON DELETE CASCADE,
+    PRIMARY KEY("parentExpenseId", "childExpenseId"),
+    CONSTRAINT ck_ids_not_equal CHECK ("parentExpenseId" <> "childExpenseId"),
+	CONSTRAINT ck_parent_not_child CHECK (exists_as_child("parentExpenseId") = FALSE),
+	CONSTRAINT ck_child_not_parent CHECK (exists_as_parent("childExpenseId") = FALSE),
+	CONSTRAINT unq_child_id UNIQUE ("childExpenseId")
+);
